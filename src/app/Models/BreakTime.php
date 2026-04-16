4f5cc8b35bac2b9bd526c683d7a254bfe5c8a7c7
keita kato <infotec62509@gmail.com>
2026-04-16 20:01:29 +0900

    モデルのリレーションを設定

--- src/app/Models/BreakTime.php
+++ src/app/Models/BreakTime.php
@@ -9,4 +9,15 @@ class BreakTime extends Model
 {
     /** @use HasFactory<\Database\Factories\BreakTimeFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'attendance_id',
+        'start_break_at',
+        'end_break_at',
+    ];
+
+    public function attendance()
+    {
+        return $this->belongsTo('App\Models\Attendance');
+    }
 }
--- src/app/Models/RevisionBreakTime.php
+++ src/app/Models/RevisionBreakTime.php
@@ -9,4 +9,15 @@ class RevisionBreakTime extends Model
 {
     /** @use HasFactory<\Database\Factories\RevisionBreakTimeFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'revision_attendance_id',
+        'start_break_at',
+        'end_break_at',
+    ];
+
+    public function revisionAttendance()
+    {
+        return $this->belongsTo('App\Models\RevisionAttendance');
+    }
 }
