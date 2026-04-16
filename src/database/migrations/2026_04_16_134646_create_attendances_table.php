0f7cd0440b1fcb39952ca845f5e4c08941f78a9b
keita kato <infotec62509@gmail.com>
2026-04-16 17:53:07 +0900

    マイグレーションファイルの作成

--- src/database/migrations/2026_04_16_134646_create_attendances_table.php
+++ src/database/migrations/2026_04_16_134646_create_attendances_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('attendances', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->dateTime('punch_in_at');
+            $table->dateTime('punch_out_at')->nullable();
+            $table->string('remarks')->nullable();
             $table->timestamps();
         });
     }
