c6cdeace98d26390d4849d553eceb5afd8054104
keita kato <infotec62509@gmail.com>
2026-05-07 05:19:21 +0900

    勤怠登録画面作成中

--- src/app/Models/Attendance.php
+++ src/app/Models/Attendance.php
@@ -10,6 +10,12 @@ class Attendance extends Model
     /** @use HasFactory<\Database\Factories\AttendanceFactory> */
     use HasFactory;
 
+    // 勤務状態定数
+    const ATTENDANCE_STATE_BEFORE_WORK = 0;     // 勤務外
+    const ATTENDANCE_STATE_WORKING = 1;         // 勤務中
+    const ATTENDANCE_STATE_BREAK_TIME = 2;      // 休憩中
+    const ATTENDANCE_STATE_FINISH_WORK = 3;     // 退勤済
+
     protected $fillable = [
         'user_id',
         'punch_in_at',
@@ -37,3 +43,20 @@ class Attendance extends Model
         return $this->hasOne(BreakTime::class)->ofMany('id', 'max');
     }
 }
+
+    public function getAttendanceState()
+    {
+        if ($this->punch_in_at->toDateString() === Carbon::now()->format('Y-m-d')) {
+            if (!empty($this->punch_out_at)) {
+                return self::ATTENDANCE_STATE_FINISH_WORK;
+            } else {
+                if(!empty($this->latestBreakTime) && empty($this->latestBreakTime['end_break_at'])) {
+                    return self::ATTENDANCE_STATE_BREAK_TIME;
+                } else {
+                    return self::ATTENDANCE_STATE_WORKING;
+                }
+            }
+        } else {
+            return self::ATTENDANCE_STATE_BEFORE_WORK;
+        }
+    }
--- src/resources/views/users/attendance.blade.php
+++ src/resources/views/users/attendance.blade.php
@@ -5,22 +5,29 @@
 @endsection
 
 @section('content')
+@php
+use App\Models\Attendance;
+@endphp
 <article class="attendance-page">
-    @if (!empty($latestAttendance) && new \Carbon\Carbon($latestAttendance['punch_in_at'])->toDateString() === now()->format('Y-m-d'))
-    @if (empty($latestAttendance['punch_out_at']) && empty($latestAttendance['latestBreakTime']['end_break_at']))
-    <!-- 休憩中 -->
-    休憩中
-    @elseif (empty($latestAttendance['punch_out_at']) && !empty($latestAttendance['latestBreakTime']['end_break_at']))
-    <!-- 出勤中 -->
-    出勤中
-    @elseif (!empty($latestAttendance['punch_out_at']))
-    <!-- 退勤済 -->
-    退勤済
-    @endif
-    @else
-    <!-- 勤務外 -->
-    <div class="attendance-state">勤務外</div>
-    <div class="attendance__date">{{ now()->format('Y年m月d日(D)') }}</div>
-    @endif
+    <section>
+        @if (empty($latestAttendance) || $latestAttendance->getAttendanceState() === Attendance::ATTENDANCE_STATE_BEFORE_WORK)
+        <div class="attendance-state">勤務外</div>
+        @elseif ($latestAttendance->getAttendanceState() === Attendance::ATTENDANCE_STATE_BREAK_TIME)
+        <div class="attendance-state">休憩中</div>
+        @elseif ($latestAttendance->getAttendanceState() === Attendance::ATTENDANCE_STATE_FINISH_WORK)
+        <div class="attendance-state">退勤済</div>
+        @elseif ($latestAttendance->getAttendanceState() === Attendance::ATTENDANCE_STATE_WORKING)
+        <div class="attendance-state">出勤中</div>
+        @endif
+        <div class="attendance__date">{{ now()->isoFormat('Y年M月D日(dd)') }}</div>
+        <div class="attendance__time">{{ now()->format('H:i') }}</div>
+    </section>
+    <section>
+        @if (empty($latestAttendance) || $latestAttendance->getAttendanceState() === Attendance::ATTENDANCE_STATE_BEFORE_WORK)
+        @elseif ($latestAttendance->getAttendanceState() === Attendance::ATTENDANCE_STATE_BREAK_TIME)
+        @elseif ($latestAttendance->getAttendanceState() === Attendance::ATTENDANCE_STATE_FINISH_WORK)
+        @elseif ($latestAttendance->getAttendanceState() === Attendance::ATTENDANCE_STATE_WORKING)
+        @endif
+    </section>
 </article>
 @endsection
