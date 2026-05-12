49effc779ba689fe7c1e998cf3d03e68c77d2507
keita kato <infotec62509@gmail.com>
2026-05-12 21:13:27 +0900

    出勤退勤ボタンの動作を作成

--- src/app/Http/Controllers/AttendanceController.php
+++ src/app/Http/Controllers/AttendanceController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use App\Models\Attendance;
 
 class AttendanceController extends Controller
 {
@@ -12,5 +14,24 @@ class AttendanceController extends Controller
         $user = Auth::user();
         $latestAttendance = $user->latestAttendance;
         return view('users.attendance', compact('latestAttendance'));
+    }
+
+    public function punchIn()
+    {
+        $newAttendance = [
+            'user_id' => Auth::id(),
+            'punch_in_at' => Carbon::now(),
+        ];
+        Attendance::create($newAttendance);
+
+        return redirect('/attendance');
+    }
+
+    public function punchOut()
+    {
+        $punchOutAt = Carbon::now();
+        Auth::user()->latestAttendance->update(['punch_out_at' => $punchOutAt]);
+
+        return redirect('/attendance');
     }
 }
