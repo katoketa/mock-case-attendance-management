3f407dc25ce30f7acef91978cd7fe90079a3304b
keita kato <infotec62509@gmail.com>
2026-05-12 21:10:32 +0900

    勤怠登録画面のルーティング

--- src/routes/web.php
+++ src/routes/web.php
@@ -9,4 +9,8 @@ Route::get('/', function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/attendance', [AttendanceController::class, 'create']);
+    Route::post('/attendance/punch_in', [AttendanceController::class, 'punchIn']);
+    Route::post('/attendance/punch_out', [AttendanceController::class, 'punchOut']);
+    Route::post('/attendance/start_break_time', [AttendanceController::class, 'startBreakTime']);
+    Route::post('attendance/end_break_time', [AttendanceController::class, 'endBreakTime']);
 });
