94596e428ff175c00dd6a578b3276830fc542cd9
keita kato <infotec62509@gmail.com>
2026-05-23 00:24:50 +0900

    勤怠一覧画面のviewを作成

--- src/resources/views/users/attendance_list.blade.php
+++ src/resources/views/users/attendance_list.blade.php
@@ -3,19 +3,18 @@
 @section('content')
 @php
 use Carbon\Carbon;
-$i = 0;
 @endphp
 <article class="default-page">
     <h1 class="default-title">勤怠一覧</h1>
     <ul class="default-change-date__ul">
         <li>
-            <a href="/attendance/list?date={{ new Carbon($selectDate)->addDay()->format('Y-m') }}" class="default-change-date__before">前月</a>
+            <a href="/attendance/list?date={{ new Carbon($selectDate)->subMonth()->format('Y-m') }}" class="default-change-date__before">前月</a>
         </li>
         <li>
-            <h2 class="default-change-date__select-date">{{ new Carbon($selectDate)->format('Y-m') }}</h2>
+            <h2 class="default-change-date__select-date">{{ new Carbon($selectDate)->format('Y/m') }}</h2>
         </li>
         <li>
-            <a href="/attendance/list?date={{ new Carbon($selectDate)->subDay()->format('Y-m') }}" class="default-change-date__after">翌月</a>
+            <a href="/attendance/list?date={{ new Carbon($selectDate)->addMonth()->format('Y-m') }}" class="default-change-date__after">翌月</a>
         </li>
     </ul>
     <table class="default-table">
@@ -27,15 +26,18 @@ $i = 0;
             <th class="default-table__th">合計</th>
             <th class="default-table__th">詳細</th>
         </tr>
+        @php
+        $i = 0;
+        @endphp
         @foreach ($attendances as $attendance)
         @while ($i <= 30)
         <tr class="default-table__tr">
             <td class="default-table__td">{{ new Carbon($selectDate)->addDays($i)->isoFormat('Y年M月D日(dd)') }}</td>
             @if (new Carbon($selectDate)->addDays($i)->format('Y-m-d') === new Carbon($attendance['punch_in_at'])->format('Y-m-d'))
-            <td class="default-table__td">{{ $attendance['punch_in_at']->format('Y-m-d') }}</td>
-            <td class="default-table__td">{{ $attendance['punch_out_at']->format('Y-m-d') }}</td>
-            <td class="default-table__td">{{ $attendance->totalBreakTime()->format('H:i') }}</td>
-            <td class="default-table__td">{{ $attendance->totalWorkTime()->format('H:i') }}</td>
+            <td class="default-table__td">{{ $attendance['punch_in_at']->format('H:i') }}</td>
+            <td class="default-table__td">{{ $attendance['punch_out_at']->format('H:i') }}</td>
+            <td class="default-table__td">{{ sprintf('%d:%02d', (int)$attendance->totalBreakTimeMinute() / 60, $attendance->totalBreakTimeMinute() % 60) }}</td>
+            <td class="default-table__td">{{ sprintf('%d:%02d', (int)$attendance->totalWorkTimeMinute() / 60, $attendance->totalWorkTimeMinute() % 60) }}</td>
             <td class="default-table__td">
                 <a href="/attendance/detail/{{ $attendance['id'] }}" class="default-table__a">詳細</a>
             </td>
