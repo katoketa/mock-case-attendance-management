cba20fb03de4fed5a37f08b56b6aa04fa50cf5a1
keita kato <infotec62509@gmail.com>
2026-06-23 21:53:36 +0900

    コントローラ分割による修正、勤怠一覧画面(管理者)のルーティング

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminAttendanceController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\RevisionAttendanceController;
@@ -10,11 +11,11 @@ Route::get('/', function () {
 });
 
 Route::middleware('auth:web')->group(function () {
-    Route::get('/attendance', [AttendanceController::class, 'create'])->name('attendance.create');
-    Route::post('/attendance/punch_in', [AttendanceController::class, 'punchIn'])->name('attendance.punch_in');
-    Route::post('/attendance/punch_out', [AttendanceController::class, 'punchOut'])->name('attendance.punch_out');
-    Route::post('/attendance/start_break_time', [AttendanceController::class, 'startBreakTime'])->name('attendance.start_break_time');
-    Route::post('/attendance/end_break_time', [AttendanceController::class, 'endBreakTime'])->name('attendance.end_break_time');
+    Route::get('/attendance', [RegisterAttendanceController::class, 'create'])->name('attendance.create');
+    Route::post('/attendance/punch_in', [RegisterAttendanceController::class, 'punchIn'])->name('attendance.punch_in');
+    Route::post('/attendance/punch_out', [RegisterAttendanceController::class, 'punchOut'])->name('attendance.punch_out');
+    Route::post('/attendance/start_break_time', [RegisterAttendanceController::class, 'startBreakTime'])->name('attendance.start_break_time');
+    Route::post('/attendance/end_break_time', [RegisterAttendanceController::class, 'endBreakTime'])->name('attendance.end_break_time');
     Route::get('/attendance/list', [AttendanceController::class, 'index'])->name('attendance.index');
     Route::get('/attendance/detail/{attendance}', [AttendanceController::class, 'show'])->name('attendance.show');
     Route::post('attendance/revision_request', [RevisionAttendanceController::class, 'store'])->name('revision_attendance.store');
@@ -29,6 +30,7 @@ Route::name('admin.')->group(function () {
     Route::middleware('auth:admin')->group(function () {
         Route::get('/admin/index', [AdminAuthController::class, 'index'])->name('index');
         Route::post('/admin/logout', [AdminAuthController::class, 'logout'])->name('logout');
-        Route::get('/admin/attendance/list', [AttendanceController::class, 'adminIndex'])->name('attendance.index');
+        Route::get('/admin/attendance/list', [AdminAttendanceController::class, 'index'])->name('attendance.index');
+        Route::get('/admin/attendance/{attendance}', [AdminAttendanceController::class, 'show'])->name('attendance.show');
     });
 });
